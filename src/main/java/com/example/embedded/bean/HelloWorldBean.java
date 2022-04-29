d41d85779114ec5e9f8ca9e8d57bfedf11138961
Ng Yat Yan <ng.yatyan@gmail.com>
2022-04-29 10:09:52 +0800

    Use JSF with Embedded Tomcat

---
+++ src/main/java/com/example/embedded/bean/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.example.embedded.bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+ 
+@ManagedBean(name="helloWorldBean")
+@RequestScoped
+public class HelloWorldBean {
+  
+    private String msg;
+ 
+    public String getMsg() {
+        return msg;
+    }
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    
+    @PostConstruct
+    private void init() { 
+        msg = "Hello World!! JFS example.. ";
+    }
+ 
+}
